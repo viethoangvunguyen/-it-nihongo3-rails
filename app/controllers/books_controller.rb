3801141159262231a26ca6bae5cf56ba311cbb22
viethoangvunguyen <viethoang.vunguyen@gmail.com>
2021-12-11 19:14:38 +0700

    ruby on rails 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
 
   # GET /books/1 or /books/1.json
   def show
+    @book_review = BookReview.new
   end
 
   # GET /books/new
@@ -64,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :number_of_pagesm, :image)
+      params.require(:book).permit(:title, :number_of_pages, :image, :detail)
     end
 end
